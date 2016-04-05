3cffc24d370792fa1c908f9d9351e487a42a7ee2
karachevtsev <karachevtsev@yahoo.com>
2016-04-05 23:02:04 +0300

    Fixed active link when scroll

--- src/js/partial/page-scroll.js
+++ src/js/partial/page-scroll.js
@@ -5,15 +5,13 @@ $(document).ready(function() {
         var offsetTop = $("#"+$(this).data("target")).offset().top - headerHeight;
         $('html, body').animate({scrollTop: offsetTop}, 1000);
     });
-
     $(document).scroll(function() {
         var offsets = [];
         $('.navigation__link').each(function(index, element) {
         offsets.push($("#" + $(element).data('target')).offset().top);
     });
-
     offsets.push( $(document).height() );
-    var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 1.5;
+    var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 5;
         for (var i = 0; i < offsets.length - 1; i++) {
           if (docScroll >= offsets[i] && docScroll < offsets[i+1]) {
             $('.navigation__link_active').removeClass('navigation__link_active');
