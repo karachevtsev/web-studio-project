36a5d6dbc7941e9a6e0ea56f2df8590fbea02174
karachevtsev <karachevtsev@yahoo.com>
2016-04-05 22:56:33 +0300

    Edit copyright text

--- src/js/partial/page-scroll.js
+++ src/js/partial/page-scroll.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     var headerHeight = $('intro').height();
-
     $(".navigation__link").click(function(e) {
         e.preventDefault();
         var offsetTop = $("#"+$(this).data("target")).offset().top - headerHeight;
@@ -8,15 +7,13 @@ $(document).ready(function() {
     });
 
     $(document).scroll(function() {
-
         var offsets = [];
         $('.navigation__link').each(function(index, element) {
         offsets.push($("#" + $(element).data('target')).offset().top);
     });
 
     offsets.push( $(document).height() );
-
-    var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 2;
+    var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 1.5;
         for (var i = 0; i < offsets.length - 1; i++) {
           if (docScroll >= offsets[i] && docScroll < offsets[i+1]) {
             $('.navigation__link_active').removeClass('navigation__link_active');
