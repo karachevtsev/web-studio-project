83ac67911fff73a0fc5f28ed6063c3b4beafad73
karachevtsev <karachevtsev@yahoo.com>
2016-04-05 23:05:06 +0300

    Edit code style in page-scroll.js

--- src/js/partial/page-scroll.js
+++ src/js/partial/page-scroll.js
@@ -8,16 +8,16 @@ $(document).ready(function() {
     $(document).scroll(function() {
         var offsets = [];
         $('.navigation__link').each(function(index, element) {
-        offsets.push($("#" + $(element).data('target')).offset().top);
-    });
-    offsets.push( $(document).height() );
-    var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 5;
+            offsets.push($("#" + $(element).data('target')).offset().top);
+        });
+        offsets.push( $(document).height() );
+        var docScroll = $(document).scrollTop() + headerHeight + $(window).height() / 5;
         for (var i = 0; i < offsets.length - 1; i++) {
-          if (docScroll >= offsets[i] && docScroll < offsets[i+1]) {
-            $('.navigation__link_active').removeClass('navigation__link_active');
-            $('.navigation__link').eq(i).addClass('navigation__link_active');
-            break;
-          };
+            if ( docScroll >= offsets[i] && docScroll < offsets[i+1] ) {
+                $('.navigation__link_active').removeClass('navigation__link_active');
+                $('.navigation__link').eq(i).addClass('navigation__link_active');
+                break;
+            };
         };
     });
 });
